722fd7215e134f395a57d7411f60cd845bea90da
kshwg <ikhwanfredy@gmail.com>
2023-06-14 20:06:09 +0700

    add login

--- app.js
+++ app.js
@@ -2,53 +2,22 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-app.use(bodyParser.json());
 
-// Data applicant (masih contoh sederhana)
-let applicant = [];
+// Set the view engine to EJS
+app.set('view engine', 'ejs');
 
-// Endpoint untuk penginputan identitas applicant
-app.post('/applicant', (req, res) => {
-  const { name, age, grade } = req.body;
-  
-  // Validasi data yang diberikan
-  if (!name || !age || !grade) {
-    return res.status(400).json({ message: 'Nama, usia, dan kelas harus diisi.' });
-  }
+// Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static('public'));
 
-  // Buat objek applicant baru
-  const newApplicant = {
-    id: applicant.length + 1,
-    name,
-    age,
-    grade
-  };
-  
-  // Tambahkan siswa baru ke dalam array
-  applicant.push(newApplicant);
+// Routes
+const indexRoute = require('./routes/index');
+const userRoute = require('./routes/user');
 
-  // Kirim respons ke client
-  res.status(201).json({ message: 'Identitas Applicant berhasil ditambahkan.', applicant: newApplicant });
-});
+app.use('/', indexRoute);
+app.use('/user', userRoute);
 
-// Endpoint untuk mendapatkan semua data applicant
-app.get('/applicant', (req, res) => {
-    res.json(applicant);
-  });
-  
-  // Endpoint untuk mendapatkan data siswa berdasarkan ID
-  app.get('/applicant/:id', (req, res) => {
-    const applicantId = parseInt(req.params.id);
-    const appl = applicant.find((applicant) => applicant.id === applicantId);
-  
-    if (!applicant) {
-      res.status(404).json({ message: 'Siswa Tidak Ditemukan!' });
-    } else {
-      res.json(appl);
-    }
-  });
-
-// Jalankan server
+// Server
 app.listen(3000, () => {
-  console.log('Server berjalan di http://localhost:3000');
+  console.log('Server is running on port 3000');
 });
